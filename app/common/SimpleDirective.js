77d52751da69f8b506df4a87bba1ea81c391d9c4
Andre Strydom <starmageza@gmail.com>
2015-04-02 10:25:50 +0200

    Cleanup and made simpler

--- app/common/SimpleDirective.js
+++ app/common/SimpleDirective.js
@@ -1,9 +1,6 @@
 class SimpleDirective {
     /*@ngInject*/
-    constructor($q, $interval) {
-        this.$q = $q;
-        this.$interval = $interval;
-
+    constructor() {
         this.restrict = 'AE'; //Properties of DDO have to be attached to the instance through this reference
         this.templateUrl = 'app/common/SimpleView.html';
         this.scope = {};
