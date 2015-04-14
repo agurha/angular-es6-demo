97ae988d47e70d5f19a8259aa4d69d4c326a1b2d
Andre Strydom <starmageza@gmail.com>
2015-04-14 11:50:08 +0200

    Remove bindToController

--- app/common/SimpleDirective/SimpleDirective.js
+++ app/common/SimpleDirective/SimpleDirective.js
@@ -4,7 +4,6 @@ class SimpleDirective {
         this.restrict = 'AE'; //Properties of DDO have to be attached to the instance through this reference
         this.templateUrl = 'app/common/Simpledirective/SimpleView.html';
         this.scope = {};
-        this.bindToController = true;
     }
 }
 
