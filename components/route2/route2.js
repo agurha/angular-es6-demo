7fbb33deb7de3d7d9d030ffe0e5fa38eaaf64fc4
Andre Strydom <starmageza@gmail.com>
2015-04-14 12:05:36 +0200

    Added sample lifecycle code to route2

--- components/route2/route2.js
+++ components/route2/route2.js
@@ -1,9 +1,23 @@
 class route2Controller {
     /*@ngInject*/
     constructor() {
-
     }
 
+    canActivate() {
+        return confirm('Are you sure you want to go to route 2 ?');
+    }
+
+    canDeactivate() {
+        return confirm('Sure you want to leave route 2 ?');
+    }
+
+    activate() {
+        console.log("route2 activated");
+    }
+
+    deactivate() {
+        console.log("route2 deactivated");
+    }
 }
 
 export default route2Controller;
