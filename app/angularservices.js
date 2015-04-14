65188a654a61f3fb7bc07db023fe6931fc2456b2
Andre Strydom <starmageza@gmail.com>
2015-04-14 11:50:28 +0200

    Removed ngResource as dependency

--- app/angularservices.js
+++ app/angularservices.js
@@ -2,7 +2,7 @@ import ng from 'angular';
 import HelloWorldService from './common/HelloWorldService';
 
 const moduleName = 'Test1App.services';
-let servicesModule = ng.module(moduleName, ['ngResource'])
+let servicesModule = ng.module(moduleName, [])
     .service('HelloWorldService', HelloWorldService);
 
 export default servicesModule;
