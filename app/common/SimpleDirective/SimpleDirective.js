886c311a0dcc76ea7ad64a0eadb59f7b41ecfd25
Andre Strydom <starmageza@gmail.com>
2015-04-20 09:24:14 +0200

    Cleanup to be more AngularJS 1.4 compliant

--- app/common/SimpleDirective/SimpleDirective.js
+++ app/common/SimpleDirective/SimpleDirective.js
@@ -4,6 +4,8 @@ class SimpleDirective {
         this.restrict = 'AE'; //Properties of DDO have to be attached to the instance through this reference
         this.templateUrl = 'app/common/Simpledirective/SimpleView.html';
         this.scope = {};
+        this.controllerAs = 'cntrl';
+        this.controller = 'Test2Controller';
     }
 }
 
