12bcc311a80e9cf32a5dd45039b2f931360d51cb
unknown <AnStrydom@ANSTRYDOMPC.metmom.mmih.biz>
2015-06-17 15:15:35 +0200

    Change browser to Chrome as PhnatomJS does not fully support bind

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,22 @@
 var istanbul = require('browserify-istanbul');
+var browserifynginject = require('browserify-nginject');
 
 module.exports = function (config) {
     config.set({
         basePath: '',
-        browsers: ['PhantomJS'],
+        browsers: ['Chrome'],  // Bombs out on PhantimJS as Phantom doe snot support bind
         frameworks: ['browserify', 'jasmine'],
         files: [
             'vendor/jQuery/2.1.3/jquery-2.1.3.min.js',
             'vendor/angular/1.4.0/angular.min.js',
             'vendor/angular/1.4.0/angular-mocks.js',
+            'vendor/angular-new-router/0.5.3/router.es5.min.js',
             'app/**/*.spec.js',
             'app/**/*.html'
         ],
         exclude: [],
         preprocessors: {
-            'app/**/*.js': ['browserify', 'ngannotate'],
+            'app/**/*.js': ['browserify'],
             "app/**/*.html": ['ng-html2js']
         },
         ngHtml2JsPreprocessor: {
@@ -38,9 +40,17 @@ module.exports = function (config) {
         },
         browserify: {
             debug: true,
-            transform: ['babelify',  istanbul({
-                ignore: ['**/*.spec.js', '**/app/utils.js']
-            })]
+            transform: [
+                ['babelify', {
+                    "optional": [
+                        "runtime"
+                    ]
+                }],
+                browserifynginject(),
+                istanbul({
+                    ignore: ['**/*.spec.js', '**/app/utils.js']
+                })
+            ]
         }
     });
 
